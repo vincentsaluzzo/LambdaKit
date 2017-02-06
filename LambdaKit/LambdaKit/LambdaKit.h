2c7fd2a0c51dd4b41320beb367bf382123fb4e13
Vincent Saluzzo <vincent.saluzzo@itelios.com>
2017-02-06 23:18:04 +0100

    add xcodeproj to add carthage compatibility

---
+++ LambdaKit/LambdaKit/LambdaKit.h
@@ -0,0 +1,19 @@
+//
+//  LambdaKit.h
+//  LambdaKit
+//
+//  Created by Vincent Saluzzo on 06/02/2017.
+//  Copyright © 2017 Lyft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LambdaKit.
+FOUNDATION_EXPORT double LambdaKitVersionNumber;
+
+//! Project version string for LambdaKit.
+FOUNDATION_EXPORT const unsigned char LambdaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LambdaKit/PublicHeader.h>
+
+
